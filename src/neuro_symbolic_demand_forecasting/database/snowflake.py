f59ae023b27c2060b40d282b82e79f065c305fa3
erewl <grunert.katrin@protonmail.com>
2024-03-25 13:33:43 +0100

    Build simple snowflake dao class

---
+++ src/neuro_symbolic_demand_forecasting/database/snowflake.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from snowflake.sqlalchemy import URL
+
+
+class SnowflakeDao:
+    engine: Engine = None
+
+    def __init__(self, url: str, uid: str, pwd: str, role: str = "DATA_ENGINEER",
+                 warehouse: str = "USER_WH"):
+        self.url = url
+        self.uid = uid
+        self.pwd = pwd
+        self.role = role
+        self.warehouse = warehouse
+
+    def connect(self):
+        self.engine = create_engine(URL(
+            account=self.url,
+            user=self.uid,
+            password=self.pwd,
+            schema='KATRIN',
+            warehouse=self.warehouse,
+            role=self.role,
+            numpy=True,
+        ))
+
+    def fetch_list(self, query: str) -> pd.DataFrame:
+        with self.engine.connect() as conn:
+            result = conn.execute(query)
+            df = pd.DataFrame(result.fetchall())
+            df.columns = result.keys()
+            return df
+
+    def fetch_one(self, query: str) -> pd.DataFrame:
+        with self.engine.connect() as conn:
+            result = conn.execute(query)
+            df = pd.DataFrame(result.fetchone())
+            df.columns = result.keys()
+            return df
--- src/neuro_symbolic_demand_forecasting/main_train.py
+++ src/neuro_symbolic_demand_forecasting/main_train.py
@@ -1,5 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+from src.neuro_symbolic_demand_forecasting.database.snowflake import SnowflakeDao
+
+
 def main_train():
-    print("Hello World!")
+    load_dotenv()
+    dao = SnowflakeDao(
+        url=os.getenv("SNOWFLAKE_URL"),
+        uid=os.getenv("SNOWFLAKE_UID"),
+        pwd=os.getenv("SNOWFLAKE_PASSWORD"),
+    )
+    dao.connect()
 
 
 if __name__ == "__main__":
