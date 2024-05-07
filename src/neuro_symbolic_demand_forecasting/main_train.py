50cf885ff8e55e0730c3674e95d23bbe765080b3
erewl <grunert.katrin@protonmail.com>
2024-05-07 14:12:19 +0200

    Create sample job for cluster, adjust main_train a bit

--- src/neuro_symbolic_demand_forecasting/main_train.py
+++ src/neuro_symbolic_demand_forecasting/main_train.py
@@ -1,6 +1,7 @@
 import argparse
 import datetime as dt
 import logging
+
 import pandas as pd
 import yaml
 from darts import TimeSeries
@@ -13,77 +14,38 @@ from neuro_symbolic_demand_forecasting.darts.loss import CustomLoss
 from sklearn.preprocessing import MinMaxScaler
 
 
-def main_train(smart_meter_files: list[str], weather_forecast_files: list[str], weather_actuals_files: list[str],
-               model_config_path: str, save_validation_forecast: bool):
-    # load_dotenv()
-    with open(model_config_path, 'r') as file:
-        logging.info(f'Loading config from {model_config_path}')
-        model_config = yaml.safe_load(file)
+def _load_csvs(_config: dict, _smart_meter_files: list[str], _weather_forecast_files: list[str],
+               _weather_actuals_files: list[str]):
+    sm = pd.read_csv(_smart_meter_files[0], index_col=None, parse_dates=['readingdate']).set_index('readingdate')
 
-    sm = pd.read_csv(smart_meter_files[0], index_col=None, parse_dates=['readingdate']).set_index('readingdate')
-
-    wf = pd.read_csv(weather_forecast_files[0], index_col=None, parse_dates=['valid_datetime']).set_index(
+    wf = pd.read_csv(_weather_forecast_files[0], index_col=None, parse_dates=['valid_datetime']).set_index(
         'valid_datetime')
-    wf = wf[model_config['weather_forecast_features']]
+    wf = wf[_config['weather_forecast_features']]
     wf = wf.resample('15min').ffill()
 
-    wa = pd.read_csv(weather_actuals_files[0], index_col=None, parse_dates=['datetime_from']).set_index('datetime_from')
-    wa = wa[model_config['weather_actuals_features']]
+    wa = pd.read_csv(_weather_actuals_files[0], index_col=None, parse_dates=['datetime_from']).set_index(
+        'datetime_from')
+    wa = wa[_config['weather_actuals_features']]
     wa = wa.resample('15min').mean()
 
-    # if we are training on the solar cluster we want -1 to 1 range, else just 0 to 1
-    smart_meter_scaler = Scaler(MinMaxScaler(feature_range=(-1, 1)))
-    smart_meter_ts = smart_meter_scaler.fit_transform(TimeSeries.from_dataframe(sm))
-
-    weather_forecast_scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
-    weather_forecast_ts = weather_forecast_scaler.fit_transform(TimeSeries.from_dataframe(wf))
-
-    weather_actuals_scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
-    weather_actuals_ts = weather_actuals_scaler.fit_transform(TimeSeries.from_dataframe(wa))
-
-    # validating datasets and (eventually adjusting)
-    logging.info(f"{len(smart_meter_ts)}, {len(weather_forecast_ts)}, {len(weather_actuals_ts)}")
-    # checking that train_meter is surrounded by the covariates
-    # print(train_meter.time_index.min(), train_meter.time_index.max())
-    # print(weather_ts.time_index.min(), weather_ts.time_index.max())
-    # print(actuals_ts.time_index.min(), actuals_ts.time_index.max())
-
-    model = init_model(model_config)
-
-    # train_meter, val_meter = meter_ts.split_after(0.8)
-    model.fit(
-        smart_meter_ts,
-        past_covariates=weather_actuals_ts,
-        future_covariates=weather_forecast_ts,
-        # val_series=val_meter,
-        # val_past_covariates=ats,
-        # val_future_covariates=wts,
-        verbose=True,
-        # trainer=pl_trainer_kwargs
-    )
-    # validate
-
-    # forecast, actual = val_meter[:-96], val_meter[-96:]
-    # # train_meter.plot()
-    # val_meter.plot()
-    #
-    # pred = model.predict(n=500, series=forecast, past_covariates=ats, future_covariates=wts)
-    pred = []
-    actual = []
-
-    print("MAPE = {:.2f}%".format(mape(actual, pred)))
-    print("SMAPE = {:.2f}%".format(smape(actual, pred)))
-    print("MAE = {:.2f}%".format(mae(actual, pred)))
+    return sm, wf, wa
 
 
-def build_torch_kwargs(kwargs: dict) -> dict:
+def _adjust_start_date(sm_ts: TimeSeries, min_weather, min_actuals) -> TimeSeries:
+    min_smart_meter = sm_ts.time_index.min()
+    overall_min_date = max(min_smart_meter, min_weather, min_actuals)
+    # set index to the latest start (min) date
+    return sm_ts[sm_ts.get_index_at_point(overall_min_date):]
+
+
+def _build_torch_kwargs(kwargs: dict) -> dict:
     if kwargs['loss_fn'] and kwargs['loss_fn'] == 'CustomLoss':
         logging.info('Loading in custom loss function')
         kwargs['loss_fn'] = CustomLoss()
     return kwargs
 
 
-def init_model(model_config: dict):
+def _init_model(model_config: dict):
     match model_config['model_class']:
         case "TFT":
             tft_config: dict = model_config['tft_config']
@@ -93,8 +55,57 @@ def init_model(model_config: dict):
                 output_chunk_length=tft_config['output_chunk_length'],
                 **{k: v for k, v in tft_config.items() if
                    k not in ['input_chunk_length', 'output_chunk_length', 'torch_kwargs']},
-                **build_torch_kwargs(tft_config['torch_kwargs'])
+                **_build_torch_kwargs(tft_config['torch_kwargs'])
             )
+
+
+def main_train(smart_meter_files: list[str], weather_forecast_files: list[str], weather_actuals_files: list[str],
+               model_config_path: str, save_validation_forecast: bool):
+    # load_dotenv()
+    logging.info('Starting training!')
+    with open(model_config_path, 'r') as file:
+        logging.info(f'Loading config from {model_config_path}')
+        model_config = yaml.safe_load(file)
+
+    # loading
+    sm, wf, wa = _load_csvs(model_config, smart_meter_files, weather_forecast_files, weather_actuals_files)
+
+    # scaling
+    weather_forecast_scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
+    weather_actuals_scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
+    smart_meter_scaler = Scaler(MinMaxScaler(feature_range=(-1, 1)))
+
+    weather_forecast_ts = weather_forecast_scaler.fit_transform(TimeSeries.from_dataframe(wf))
+    weather_actuals_ts = weather_actuals_scaler.fit_transform(TimeSeries.from_dataframe(wa))
+    smart_meter_ts = smart_meter_scaler.fit_transform(TimeSeries.from_dataframe(sm))
+    smart_meter_ts = _adjust_start_date(smart_meter_ts, weather_forecast_ts.time_index.min(),
+                                        weather_actuals_ts.time_index.min())
+
+    # training
+    model = _init_model(model_config)
+    train_meter, val_meter = smart_meter_ts.split_after(0.8)
+    model.fit(
+        train_meter,
+        past_covariates=weather_actuals_ts,
+        future_covariates=weather_forecast_ts,
+        val_series=val_meter,
+        val_past_covariates=weather_actuals_ts,
+        val_future_covariates=weather_forecast_ts,
+        verbose=True,
+        # trainer=pl_trainer_kwargs # would be nice to have early stopping here
+    )
+    # validate
+
+    forecast, actual = val_meter[:-96], val_meter[-96:]
+    # # train_meter.plot()
+    # val_meter.plot()
+    #
+    pred = model.predict(n=96, series=forecast, past_covariates=weather_actuals_ts,
+                         future_covariates=weather_forecast_ts)
+
+    logging.info("MAPE = {:.2f}%".format(mape(actual, pred)))
+    logging.info("SMAPE = {:.2f}%".format(smape(actual, pred)))
+    logging.info("MAE = {:.2f}%".format(mae(actual, pred)))
 
 
 if __name__ == "__main__":
@@ -120,5 +131,4 @@ if __name__ == "__main__":
 
     save_validation_forecast = True
 
-    logging.info('Starting training!')
     main_train(smd_files, wfd_files, wad_files, args.model_configuration, save_validation_forecast=True)
