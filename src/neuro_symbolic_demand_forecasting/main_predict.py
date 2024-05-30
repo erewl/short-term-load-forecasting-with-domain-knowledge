e71b34c01e1ad539066470ef9e7529cbb6b4b35f
erewl <grunert.katrin@protonmail.com>
2024-05-30 15:21:19 +0200

    Restructure main_predict

--- src/neuro_symbolic_demand_forecasting/main_predict.py
+++ src/neuro_symbolic_demand_forecasting/main_predict.py
@@ -3,88 +3,92 @@ import datetime as dt
 
 import argparse
 import pickle
+from typing import Optional
 
 import pandas as pd
+import torch
 import yaml
 from darts import TimeSeries
-from darts.dataprocessing.transformers import Scaler
 from darts.models.forecasting.torch_forecasting_model import TorchForecastingModel
-from sklearn.preprocessing import MinMaxScaler
+from darts.metrics.metrics import mae, mape, rmse, smape, r2_score
 
-from neuro_symbolic_demand_forecasting.main_train import load_csvs, create_timeseries_from_dataframes, AMS_TZ
+from neuro_symbolic_demand_forecasting.main_train import load_csvs, create_timeseries_from_dataframes
 
 
 def main_evaluate(_config: dict, start_date: str, end_date: str,
                   smart_meter_data_files: str,
                   weather_forecast_file: str, weather_actuals_file: str, model_folder: str):
-    sms, wf, wa = load_csvs(_config, [smart_meter_data_files], [weather_forecast_file], [weather_actuals_file])
-    end_date_dt = pd.to_datetime(end_date).tz_localize(AMS_TZ)
-    actuals = sms[0][end_date_dt:end_date_dt + pd.Timedelta(days=7)]
+    # determine which scaler to use for Smart meter scaling
+    # start and end should be within the range of the smartmeter data file
+    # calculate metrics per day, but also for the entire timerange and write to file.
 
-    sms = [s[:end_date] for s in sms]
-
-    model = TorchForecastingModel.load(model_folder + '/train_model.pkl')
+    # TODO take mutliple model_folders as input and iterate over them
+    model = TorchForecastingModel.load(model_folder + '/train_model.pkl', map_location=torch.device('cpu'))
+    model.to_cpu()
     logging.info(f'Loaded successful model of class {type(model)}')
-
-    sm_tss, wf_ts, wa_ts = create_timeseries_from_dataframes(sms, wf, wa, add_static_covariates=model.uses_static_covariates, scale=False)
-    smart_meter_ts = sm_tss[0]
-
-
-    non_pv_scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
-    pv_scaler = Scaler(MinMaxScaler(feature_range=(-1, 1)))
-
-    weather_actuals_scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
-    weather_forecast_scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
 
     with open(f'{model_folder}/scalers.pkl', 'rb') as f:
         non_pv_scaler = pickle.load(f)
         pv_scaler = pickle.load(f)
+
         weather_actuals_scaler = pickle.load(f)
         weather_forecast_scaler = pickle.load(f)
+        logging.info("Loaded the scalers successfully")
 
+    sms, wf, wa = load_csvs(_config, [smart_meter_data_files], [weather_forecast_file], [weather_actuals_file])
+    sm_tss, wf_ts, wa_ts = create_timeseries_from_dataframes(sms, wf, wa,
+                                                             add_static_covariates=model.uses_static_covariates,
+                                                             scale=False)
+    assert len(sm_tss) == 1
+    sm_tss = sm_tss[0]
+    if any(sm_tss.min(axis=0) < 0):
+        scaler = pv_scaler
+    else:
+        scaler = non_pv_scaler
 
+    scaled_smart_meter_ts = scaler.fit_transform(sm_tss)
     scaled_weather_actuals = weather_actuals_scaler.transform(wa_ts)
     scaled_weather_forecasts = weather_forecast_scaler.transform(wf_ts)
 
-    if any(smart_meter_ts.min(axis=0) < 0):
-        scaler = pv_scaler
-        results_file_name = "val_results_pv"
-    else:
-        scaler = non_pv_scaler
-        results_file_name = "val_results_non_pv"
-
-    scaled_smart_meter_ts = scaler.transform(smart_meter_ts)
-    scaled_actuals = scaler.transform(TimeSeries.from_dataframe(actuals))
-
-    if model.supports_future_covariates and model.supports_past_covariates:
-        logging.info("Running prediction with full covariates")
-        prediction = model.predict(
-            n=672,
-            series=scaled_smart_meter_ts,
+    ts: Optional[TimeSeries] = None
+    for d in pd.date_range(start_date, end_date):  # could also be pass as an array of timeseries to the model.
+        input_start, input_end = d - dt.timedelta(days=7), d - dt.timedelta(minutes=15)
+        scaled_input = scaled_smart_meter_ts[input_start:input_end]
+        forecast_start, forecast_end = d, d + dt.timedelta(days=1) - dt.timedelta(minutes=15)
+        logging.info(f"Using {input_start} to {input_end} to forecast {forecast_start} to {forecast_end}")
+        scaled_actuals = scaled_smart_meter_ts[forecast_start:forecast_end]
+        scaled_predictions = model.predict(
+            n=model.output_chunk_length,
+            series=scaled_input,
             past_covariates=scaled_weather_actuals,
             future_covariates=scaled_weather_forecasts
         )
-    elif model.supports_future_covariates:
-        logging.info("Running prediction with future covariates")
-        prediction = model.predict(
-            n=672,  # TODO end_date - start_date days hours * 4 for PTUs
-            series=sm_tss,
-            future_covariates=wf_ts
-        )
-    else:
-        logging.info("Running prediction with no covariates")
-        prediction = model.predict(
-            n=96,  # TODO end_date - start_date days hours * 4 for PTUs
-            series=sm_tss
-        )
+        rescaled_predictions = scaler.inverse_transform(scaled_predictions).with_columns_renamed('gross', 'gross_predicted')
+        rescaled_actuals = scaler.inverse_transform(scaled_actuals).with_columns_renamed('gross', 'gross_actuals')
+        if ts is None:
+            ts = rescaled_predictions.stack(rescaled_actuals)
+        else:
+            ts = ts.append(rescaled_predictions.stack(rescaled_actuals))
+    get_metrics(ts['gross_actuals'], ts['gross_predicted'])
+    ts.to_csv(f'{model_folder}/evaluation_data.csv')
 
-    rescaled_prediction = scaler.inverse_transform(prediction)
-    rescaled_actuals = scaler.inverse_transform(scaled_actuals)
 
-    with open(model_folder + f'/{results_file_name}_pre_scaled.pkl', 'wb') as f:
-        pickle.dump(rescaled_prediction, f)
-        pickle.dump(rescaled_actuals, f)
-    # evaluate
+def get_metrics(_target, _predicted):
+    # sMAPE
+    smape_ = smape(_target, _predicted)
+    mape_ = mape(_target, _predicted)
+    mae_ = mae(_target, _predicted)
+    # RMSE
+    rmse_ = rmse(_target, _predicted)
+    r2_ = r2_score(_target, _predicted)
+
+    logging.info(f'''
+        SMAPE {round(smape_, 3)}
+        MAPE  {round(mape_, 3)}
+        MAE   {round(mae_, 3)}
+        RMSE  {round(rmse_, 3)}
+        R2    {round(r2_, 3)}
+        ''')
 
 
 if __name__ == "__main__":
@@ -113,7 +117,7 @@ if __name__ == "__main__":
     # start_date = dt.datetime(2023, 7, 8, 0, 0, tzinfo=AMS_TZ)
     # end_date = dt.datetime(2023, 7, 14, 23, 45, tzinfo=AMS_TZ)
 
-    main_evaluate(_config=model_config, start_date='2023-07-15 00:00', end_date='2023-07-22 23:45',
+    main_evaluate(_config=model_config, start_date='2023-07-15', end_date='2023-07-22',
                   smart_meter_data_files=args.smart_meter_data, weather_forecast_file=args.weather_forecast_data,
                   weather_actuals_file=args.weather_actuals_data,
                   model_folder=args.save_model_as)
